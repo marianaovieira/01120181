7e60866e69e07a9c43d25e85101a98490f127de2
Mariana Vieira <marianaovieira1@gmail.com>
2018-05-24 17:52:24 -0300

    Cálculos 1 e 2

--- app/src/main/java/pooa2018/iff/br/trabalho01120181/MainActivity.java
+++ app/src/main/java/pooa2018/iff/br/trabalho01120181/MainActivity.java
@@ -1,52 +1,61 @@
 package pooa2018.iff.br.trabalho01120181;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtArea;
+    private TextView lPropostas;
+    private Button bOK;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        txtArea = findViewById(R.id.txtResposta);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        bOK.setOnClickListener(new View.OnClickListener() {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
+        @Override
+        public void onClick (View view){
+            calcular();
+        }
+        )
+    }
 
-        return super.onOptionsItemSelected(item);
+        private void calcular(){
+
+        double area = Float.parseFloat(txtArea.getText().toString());
+        double total_litros = 0, preco_L, preco_G;
+        int total_latas = 0, total_galoes = 0;
+
+        total_litros = area / 6;
+
+        total_latas = (int) Math.ceil(total_litros / 18);
+        total_galoes = (int) Math.ceil(total_litros / 3.6);
+
+        preco_L = total_latas * 80;
+        preco_G = total_galoes * 25;
+
+
+        //Ifs de condicionamento
+
+
+
+        String msg1 = "A quantidade de latas de tinta utilizadas serão" + total_latas + " com preço total de R$" + preco_L + ". \n A quantidade de galões de tinta utilizadas serão:" + total_galoes + " com preço total de R$" + preco_L;
+        String msg2 = " ";
+        int qtd_L, qtdG;
+
+        lPropostas.setText((String.valueOf(msg1).concat(msg2)));
+
     }
 }
